91c26e1d82fbfbd77f57fdb99bd516807dcedae5
Minatozaki <130718064+lMinatozaki@users.noreply.github.com>
2024-08-13 17:36:19 -0400

    Add files via upload

---
+++ db.py
@@ -0,0 +1,9 @@
+import pymongo
+
+client = pymongo.MongoClient("localhost", 27017)
+
+db = client['DoofenshmirtzDaniela']
+
+examCollection = db['examenes']
+userCollection = db['users']
+categoryCollection = db['categorias']
