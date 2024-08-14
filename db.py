9102b8386a2b8e55a9014dc2e17cefe779426c25
Minatozaki <130718064+lMinatozaki@users.noreply.github.com>
2024-08-14 18:11:23 -0400

    Delete db.py
    Delete to update again

--- db.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-
-client = pymongo.MongoClient("localhost", 27017)
-
-db = client['DoofenshmirtzDaniela']
-
-examCollection = db['examenes']
-userCollection = db['users']
-categoryCollection = db['categorias']
